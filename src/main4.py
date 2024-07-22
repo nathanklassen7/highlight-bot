3734218e5fcb2824a4ff6a2143983f85aa0b71ff
Nathan Klassen <nathan.klassen@faire.com>
2024-07-22 15:02:29 -0400

    wip

--- src/main4.py
+++ src/main4.py
@@ -7,17 +7,15 @@ from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import CircularOutput
 
-lsize = (320, 240)
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration({"size": (1280, 720), "format": "RGB888"})
 picam2.configure(video_config)
 picam2.start_preview()
 encoder = H264Encoder(1000000, repeat=True)
-encoder.output = CircularOutput()
+encoder.output = CircularOutput(buffersize=300, outputtofile=False)
 picam2.start()
 picam2.start_encoder(encoder)
 
-w, h = lsize
 prev = None
 encoding = False
 ltime = 0
