b165957032609ffc27761287b5f5617f6561232f
Nathan Klassen <nathan.klassen@faire.com>
2024-07-22 14:18:45 -0400

    wip

--- src/main3.py
+++ src/main3.py
@@ -1,14 +1,19 @@
+import time
+
+from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import CircularOutput
-from picamera2 import Picamera2
-import time
+
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration())
+fps = 30
+dur = 5
+micro = int((1 / fps) * 1000000)
+vconfig = picam2.create_video_configuration()
+vconfig['controls']['FrameDurationLimits'] = (micro, micro)
+picam2.configure(vconfig)
 encoder = H264Encoder()
-output = CircularOutput()
-picam2.start_recording(encoder, output)
-# Now when it's time to start recording the output, including the previous 5 seconds:
+output = CircularOutput(buffersize=int(fps * (dur + 0.2)), outputtofile=False)
 output.fileoutput = "file.h264"
-output.start()
-time.sleep(10)
+picam2.start_recording(encoder, output)
+time.sleep(dur)
 output.stop()
