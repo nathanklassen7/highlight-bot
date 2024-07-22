8d1463f459744008ecbeac8fc8033fcd5aa6b43a
Nathan Klassen <nathan.klassen@faire.com>
2024-07-22 14:29:14 -0400

    wip

--- src/main3.py
+++ src/main3.py
@@ -11,7 +11,7 @@ micro = int((1 / fps) * 1000000)
 vconfig = picam2.create_video_configuration({"size": (1536, 864)})
 picam2.configure(vconfig)
 encoder = H264Encoder()
-output = CircularOutput(buffersize=int(fps * (dur + 0.2)), outputtofile=False)
+output = CircularOutput(buffersize=int(fps * (dur + 0.2)))
 output.fileoutput = "file.h264"
 picam2.start_recording(encoder, output)
 time.sleep(dur)
