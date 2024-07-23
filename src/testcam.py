54dca4bb7484929229c879485b39523444cfadd8
Nathan Klassen <nathanklassen7@gmail.com>
2024-07-23 12:27:33 -0400

    working prototype

--- src/testcam.py
+++ src/testcam.py
@@ -1,11 +1,15 @@
-from picamera2 import Picamera2, Preview
+from picamera2 import Picamera2
+import time
+from picamera2.encoders import H264Encoder, Quality
+from subprocess import call
 
-def activate_camera():
-    picam2 = Picamera2()
-    camera_config = picam2.create_video_configuration({"size": (1536, 864), "format": "RGB888"})
-    # camera_config = picam2.create_video_configuration()
-    picam2.configure(camera_config)
-    picam2.start_preview(Preview.NULL)
-    picam2.set_controls({"ExposureTime":10000})
-    picam2.start()
-    return picam2
+picam2 = Picamera2()
+
+camera_config = picam2.create_video_configuration(controls={"FrameRate": 60},main={"size":(1536,864)})
+picam2.configure(camera_config)
+encoder = H264Encoder()
+picam2.start_recording(encoder, 'test.h264', quality=Quality.HIGH)
+time.sleep(5)
+picam2.stop_recording()
+call([f"ffmpeg -r {60} -i test.h264 -c copy test.mp4 -y"], shell=True)
+call([f"rm test.h264"],shell=True)
--- src/upload_with_slack.py
+++ src/upload_with_slack.py
@@ -1,28 +1,42 @@
 
+from consts import CHANNEL_ID, CLIP_DIRECTORY
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from flask import Flask, request, Response
+import os
+from subprocess import call
 
-client = WebClient(token=SLACK_BOT_TOKEN)
-
-def upload_video(filename):
+def upload_all_videos(client, channel, ts):
+    files = os.listdir('clips')
+    files.sort()
+    file_uploads = [{"file":CLIP_DIRECTORY+file,"title":file} for file in files]
+    print(file_uploads)
     try:
-        response = client.files_upload(
-            channels=SLACK_CHANNEL_ID,
-            file=filename,
-            initial_comment="Here's the latest 30 seconds of footage!"
+        response = client.files_upload_v2(
+            channels=channel,
+            file_uploads=file_uploads,
+            initial_comment="Here's the latest 30 seconds of footage!",
+            thread_ts=ts
         )
         print(response)
+        call([f'rm -rf clips'],shell=True)
     except SlackApiError as e:
         print(f"Error uploading file: {e.response['error']}")
+        
 
-def handle_message(event_data):
-    message_text = event_data['text']
-    if '/highlight' in message_text:
-        # Save buffered frames to a video file
-        out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (640, 480))
-        for frame in video_buffer:
-            out.write(frame)
-        out.release()
-
-        # Upload the saved video file to Slack
-        upload_video('output.avi')
+def upload_videos_sequentially(client, channel, ts):
+    files = [filename for filename in os.listdir('clips')]
+    files.sort()
+    
+    try:
+        for file in files:
+            response = client.files_upload_v2(
+                channels=channel,
+                file=CLIP_DIRECTORY+file,
+                initial_comment=file,
+                thread_ts=ts
+            )
+            print(response)
+        call([f'rm -rf clips'],shell=True)
+    except SlackApiError as e:
+        print(f"Error uploading file: {e.response['error']}")
