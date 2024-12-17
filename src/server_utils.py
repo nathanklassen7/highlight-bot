7e2a073eecb9e98242c49d1ca773a27ce7371b30
Nathan Klassen <nathan.klassen@faire.com>
2024-12-17 17:01:49 -0500

    wip

--- src/server_utils.py
+++ src/server_utils.py
@@ -26,12 +26,19 @@ class ResponseFunctions:
             text=message,
         )
         
-    def send_message_with_blocks(self, blocks: list) -> dict:
-        return client.chat_postEphemeral(
-            channel=self.channel_id,
-            user=self.user_id,
-            blocks=blocks
-        )
+    def send_message_with_blocks(self, blocks: list, timestamp: str | None = None) -> dict:
+        if timestamp is None:
+            return client.chat_postEphemeral(
+                channel=self.channel_id,
+                user=self.user_id,
+                blocks=blocks,
+            )
+        else:
+            return client.chat_update(
+                channel=self.channel_id,
+                ts=timestamp,
+                blocks=blocks,
+            )
         
     def send_ephemeral(self, message: str) -> dict:
         return client.chat_postEphemeral(
@@ -67,6 +74,6 @@ def get_message(channel: str, timestamp: str):
 def update_message_blocks(channel: str, timestamp: str, blocks: list):
     return client.chat_update(
         channel=channel,
-        timestamp=timestamp,
+        ts=timestamp,
         blocks=blocks
     )
