482e9d2f34eb4dae5695611b363366a5b08db6a8
Nathan Klassen <60327660+nathanklassen7@users.noreply.github.com>
2025-03-14 12:01:00 -0400

    make audio mono (#8)

--- src/encoding_utils.py
+++ src/encoding_utils.py
@@ -7,4 +7,4 @@ def encode_video(audio_start_time, video_start_time):
     offset = int((audio_start_time - video_start_time)*1000)
     timestamp = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
     output_file = f'clips/{timestamp}.mp4'
-    return call([f'ffmpeg -r {fps} -i {VIDEO_BUFFER_FILE} -i {AUDIO_BUFFER_FILE} -c:v copy -af "adelay={offset}|{offset}" -c:a aac {output_file} -y -shortest'], shell=True,stdout=DEVNULL, stderr=DEVNULL)
+    return call([f'ffmpeg -r {fps} -i {VIDEO_BUFFER_FILE} -i {AUDIO_BUFFER_FILE} -c:v copy -af "adelay={offset}|{offset},pan=mono|c0=FL" -c:a aac -ac 1 {output_file} -y -shortest'], shell=True,stdout=DEVNULL, stderr=DEVNULL)
