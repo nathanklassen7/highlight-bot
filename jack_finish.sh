352c9dc21bea4a93e0d083b1d6be0d3be91f94e9
Nathan Klassen <nathan.klassen@faire.com>
2026-03-20 11:50:18 -0400

    more compatibility

--- jack_finish.sh
+++ jack_finish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname -- "$0")" && pwd)"
 date +%s.%N > "${SCRIPT_DIR}/time.tme"
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname -- "$0")" && pwd)"
 set -a
 source "$SCRIPT_DIR/.env"
 set +a
