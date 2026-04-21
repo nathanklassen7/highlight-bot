a0a095b27ef2b4e7fb4d849bc3b93417eb366e41
Nathan Klassen <nathan.klassen@faire.com>
2026-04-21 16:34:58 -0400

    more fix?

--- frontend/src/components/viewer/VideoPlayer.tsx
+++ frontend/src/components/viewer/VideoPlayer.tsx
@@ -7,14 +7,18 @@ interface VideoPlayerProps {
 export const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
   function VideoPlayer({ filename }, ref) {
     return (
-      <div className="relative w-full pt-[56.25%]">
+      <div
+        className="relative w-full pt-[56.25%] bg-black select-none"
+        style={{ WebkitTouchCallout: "none", WebkitUserSelect: "none" }}
+      >
         <video
           ref={ref}
           controls
           playsInline
-          preload="metadata"
+          preload="auto"
           {...{ "webkit-playsinline": "true" }}
           className="absolute top-0 left-0 w-full h-full object-contain bg-black"
+          style={{ WebkitTouchCallout: "default" }}
         >
           <source src={`/clips/file/${filename}`} type="video/mp4" />
         </video>
